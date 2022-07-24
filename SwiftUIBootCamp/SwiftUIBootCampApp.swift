4010ee97897e5a6d28e9dd45001fa4a6b9b76661
Brian Vieira <brian.vieira@ebankit.com>
2022-07-24 16:47:24 +0100

    Initial Commit

---
+++ SwiftUIBootCamp/SwiftUIBootCampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBootCampApp.swift
+//  SwiftUIBootCamp
+//
+//  Created by Brian Michael on 24/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBootCampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
